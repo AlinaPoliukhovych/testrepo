08594742f172e40882f157810d17264c97d31551
AlinaPoliukhovych <56222727+AlinaPoliukhovych@users.noreply.github.com>
2020-08-31 11:51:48 +0300

    add ...

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             int x = 8;
-            Console.WriteLine(x+78);
+            Console.WriteLine(x+78+56);
         }
     }
 }
